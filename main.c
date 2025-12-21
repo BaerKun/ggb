8442f4154cea13375f5732528298fa395adc1496
Bear Kun <bear_kun@outlook.com>
2025-12-21 18:08:25 +0800

    [fix] cmake link raylib

--- main.c
+++ main.c
@@ -1,20 +1,9 @@
-#include "gbb_gl.h"
-#include "geometry.h"
 #include "console.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-Window *mainWindow, *imageWindow;
-Point2i origin = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
-
 int main() {
-  graphicalInit();
-
-  mainWindow = getNewWindow("GGB", WINDOW_WIDTH, WINDOW_HEIGHT);
-  imageWindow = getSubWindow(mainWindow, 0, 0, WINDOW_WIDTH,
-                             WINDOW_HEIGHT);
-
   console_mode();
   return 0;
 }
